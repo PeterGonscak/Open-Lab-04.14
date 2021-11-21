ae101a04b3f7a0d8f9dcb1c82d40d4ad17cf881e
Peter Gonščák <peter.gonscak190@gmail.com>
2021-11-21 20:00:13 +0100

    donee

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -6,7 +6,29 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+            int a = 0; 
+            foreach (string item in strings) 
+            { 
+                if (a < item.Length) a = item.Length; 
+            }
+            for (int i = 0; i < a + 3; i++) 
+                Console.Write("*"); 
+            Console.WriteLine("*"); 
+            foreach (string item in strings) 
+            { 
+                if (item.Length < a) 
+                {
+                    var s = "";
+                    for (int i = 0; i < a - item.Length; i++) 
+                        s += " ";
+                    Console.WriteLine("* " + item + s + " *");
+                } 
+                else 
+                    Console.WriteLine("* " + item + " *"); 
+            }
+            for (int i = 0; i < a + 3; i++) 
+                Console.Write("*"); 
+            Console.WriteLine("*");
         }
     }
 }
